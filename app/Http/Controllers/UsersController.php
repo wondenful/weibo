3bb33bea0328cb7781c48f1b27254e6d82338329
WangJ <wangjinqiang@lf-network.com>
2025-02-26 08:51:15 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,11 +19,27 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request, [
+//      Laravel 8.0版本修改
+        $validatedData = $request->validate([
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+//        Laravel 9.0版本修改
+//        $this->validate($request, [
+//            'name' => 'required|unique:users|max:50',
+//            'email' => 'required|email|unique:users|max:255',
+//            'password' => 'required|confirmed|min:6'
+//        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+
+Route::resource('users', UsersController::class);
 
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
-Route::resource('users', 'UsersController');
+//Route::resource('users', 'UsersController');
