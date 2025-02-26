d82439451838f88e103c4a97096887f2ada28fd7
WangJ <wangjinqiang@lf-network.com>
2025-02-26 12:02:59 +0000

    用户激活

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
